c69099a5c860f748664b45a544f1bf7ca5fa18b7
Samkaxe <must2765@easv365.dk>
2024-04-16 20:02:36 +0200

    fixing db

--- PatientDatabase/PatientDbContext.cs
+++ PatientDatabase/PatientDbContext.cs
@@ -8,7 +8,9 @@ public class PatientDbContext : DbContext
     public DbSet<Patient> Patients { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite("Data Source=patient.db");
+    { 
+       
+        optionsBuilder.UseSqlite("Data Source=../PatientDatabase/patient.db");
+        //optionsBuilder.UseSqlite("Data Source=C:\\Users\\charl\\RiderProjects\\Compulsoryassignment2\\PatientDatabase\\patient.db");
     }
 }
