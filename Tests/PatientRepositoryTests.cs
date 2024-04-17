9b4fe80985dcf91ff263f1dcad92f9fd9e443555
Renaldinho <rena0968@easv365.dk>
2024-04-17 17:04:53 +0200

    .

---
+++ Tests/PatientRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Core;
+using Moq;
+using Moq.EntityFrameworkCore;
+using PatientDatabase;
+
+namespace Tests;
+
+public class PatientRepositoryTests
+{
+    [Fact]
+    public void GetAllPatients_ReturnsAllPatientsFromContext()
+    {
+        var mockData = new List<Patient>
+        {
+            new Patient
+            {
+                Email = "bobby@email.com",
+                Measurements = new List<Measurements>(),
+                Name = "Bob",
+                SSN = "AAA-GG-SSSS"
+            },
+            new Patient
+            {
+                Email = "bobby@hotmail.com",
+                Measurements = new List<Measurements>(),
+                Name = "Bob's 2nd degree brother",
+                SSN = "YSG-AF-1234"
+            },
+            new Patient
+            {
+                Email = "paperboi@email.com",
+                Measurements = new List<Measurements>(),
+                Name = "Alfred",
+                SSN = "AAA-GG-SSSS"
+            }
+        };
+
+        var mockContext = new Mock<PatientDbContext>();
+        mockContext.Setup(context => context.Patients)
+            .ReturnsDbSet(mockData);
+        
+        // var repository = new PatientC
+    }
+}
